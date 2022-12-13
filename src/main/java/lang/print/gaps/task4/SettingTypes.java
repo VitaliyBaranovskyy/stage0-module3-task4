8fcd31bc5eca461453a666fa45ef3dce9d1066e6
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2022-12-13 15:02:15 +0200

    Update SettingTypes.java

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-        long first = 10_000_000_000L;
+        long first = 10000000000L;
         char second = 'a';
         boolean third = 5 > 9;
         double forth = 12.90434323324342;
