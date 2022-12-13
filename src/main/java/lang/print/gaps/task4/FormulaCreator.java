9be4abcb279756c7520c866f92c354acb1d435b6
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2022-12-13 15:30:25 +0200

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -9,6 +9,6 @@ public class FormulaCreator {
         System.out.println(a);
         System.out.println(b);
         System.out.println("(9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)");
-        System.out.println((9*a*a-5*b+2+a-7)((a+b-4*a*b)/2));
+        System.out.println((9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2));
     }
 }
